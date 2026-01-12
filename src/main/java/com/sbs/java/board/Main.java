0de5763f77ebb674d81ff118e5a3d5dd12f10494
maxwell0212 <maxwell0212@hotmail.com>
2026-01-12 21:39:59 +0900

    간단한 명령어 입력 받기

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -1,12 +1,23 @@
 package com.sbs.java.board;
 
+import java.util.Scanner;
+
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("== 자바 게시판 ==");
         System.out.println("게시판을 시작합니다.");
+
+        System.out.print("명령) ");
+        String cmd = sc.nextLine();
+        System.out.printf("입력받은 명령어 : %s \n", cmd);
+
         System.out.println("게시판을 종료합니다.");
         System.out.println("== 자바 게시판 종료 ==");
+
+        sc.close();
     }
 }
