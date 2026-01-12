a1657921ab96518c90cd374cc878c0c6d953150c
maxwell0212 <maxwell0212@hotmail.com>
2026-01-12 21:45:07 +0900

    exit 명령어를 받으면 게시판 종료.

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -11,11 +11,18 @@ public class Main {
         System.out.println("== 자바 게시판 ==");
         System.out.println("게시판을 시작합니다.");
 
-        System.out.print("명령) ");
-        String cmd = sc.nextLine();
-        System.out.printf("입력받은 명령어 : %s \n", cmd);
+        while(true){
+            System.out.print("명령) ");
+            String cmd = sc.nextLine();
+            if( cmd.equals("exit")){
+                System.out.println("게시판을 종료합니다.");
+                break;
+            }
 
-        System.out.println("게시판을 종료합니다.");
+            System.out.printf("입력받은 명령어 : %s \n", cmd);
+
+        }
+
         System.out.println("== 자바 게시판 종료 ==");
 
         sc.close();
