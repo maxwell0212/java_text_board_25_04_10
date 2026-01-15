8a9348610d08b622b40b2e4bb984f342098bc366
maxwell0212 <maxwell0212@hotmail.com>
2026-01-15 13:46:13 +0900

    게시물 테스트 데이터 생성

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -3,15 +3,27 @@ package com.sbs.java.board;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
+    static void article_test_data(List<Article> articles) {
+        /*
+        articles.add(new Article(1, "제목1", "내용1"));
+        articles.add(new Article(2, "제목2", "내용2"));
+        articles.add(new Article(3, "제목3", "내용3"));
+        */
+        IntStream.rangeClosed(1, 3)
+                .forEach(i -> articles.add(new Article(i, "제목"+i, "내용"+i) )  );
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int lastArticleId = 0;
         Article lastArticle = null;
         List<Article> articles = new ArrayList<>();
+        article_test_data(articles);
 
         System.out.println("== 자바 게시판 ==");
         System.out.println("게시판을 시작합니다.");
