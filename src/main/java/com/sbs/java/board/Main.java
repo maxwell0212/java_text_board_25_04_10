a2760ae60b6358f686b723b6e1943f66f385cfcd
maxwell0212 <maxwell0212@hotmail.com>
2026-01-15 14:20:19 +0900

    게시물 리스트 출력시 최신 게시물 순으로 출력

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -82,19 +82,12 @@ public class Main {
                 }
                 System.out.println("== 게시물 리스트 ==");
                 System.out.println("번호 | 제목");
-                /*
-                for(int i=0; i<articles.size(); i++){
+
+                for(int i=articles.size()-1; i>=0; i--){
                     Article article = articles.get(i);
                     System.out.printf("%d | %s\n", article.id, article.subject);
                 }
-                */
-                /*
-                for( Article article : articles){
-                    System.out.printf("%d | %s\n", article.id, article.subject);
-                }
-                */
-                articles.forEach( article ->
-                        System.out.printf("%d | %s\n", article.id, article.subject));
+
             }
             else {
                 System.out.println("잘못된 명령어 입니다.");
