4db70d85d1b3829567004bb4cc1dc6ba25834dde
maxwell0212 <maxwell0212@hotmail.com>
2026-01-17 11:26:46 +0900

    분석한 파라미터를 리스트가 아닌 맵에 저장

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,15 +1,12 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class AppTest {
     public static void main(String[] args) {
-        String queryString = "a=1&b=2&c=3&d=4";
+        String queryString = "subject=제목1&content=내용1&writerName=양관식&boardId=5";
         String[] queryStringBits = queryString.split("&");
         System.out.println(Arrays.toString(queryStringBits) );
 
-        List<String> paramNames = new ArrayList<>();
-        List<Integer> paramValues = new ArrayList<>();
+        Map<String, String> params = new HashMap<>();
 
         for(String bit : queryStringBits) {
             String[] bitBits = bit.split("=");
@@ -17,22 +14,19 @@ public class AppTest {
             String paramName = bitBits[0];
             String paramValue = bitBits[1];
 
-            paramNames.add(paramName);
-            paramValues.add(Integer.parseInt(paramValue));
-
+            params.put(paramName, paramValue);
             //System.out.printf("%s : %s\n", paramName, paramValue );
         }
+        System.out.println(params);
 
-        /*
-        Arrays.stream(queryStringBits)
-                .forEach(System.out::println);
-         */
+        System.out.println("== 원하는 것만 하나씩 뽑아오기 ==");
+        System.out.printf("subject : %s\n", params.get("subject"));
+        System.out.printf("content : %s\n", params.get("content"));
+        System.out.printf("writerName : %s\n", params.get("writerName"));
+        System.out.printf("boardId : %s\n", params.get("boardId"));
 
-        for(int i=0; i<paramNames.size(); i++) {
-            String paramName = paramNames.get(i);
-            int paramValue = paramValues.get(i);
-            System.out.printf("%s : %d\n", paramName, paramValue);
-        }
+        System.out.println("== 반복문을 이용한 데이터 순회 ==");
+        params.forEach((key, value)-> System.out.printf("%s : %s\n", key, value));
     }
 
 }
