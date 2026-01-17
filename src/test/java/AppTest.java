b4da80a63e076f3cb0482fcba9d5b433ef5514d3
maxwell0212 <maxwell0212@hotmail.com>
2026-01-17 11:03:34 +0900

    리스트 2개를 이용하여 파라미터 이름, 값 따로 담기

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,13 +1,15 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class AppTest {
     public static void main(String[] args) {
-        String queryString = "a=1&b=2&c=3";
+        String queryString = "a=1&b=2&c=3&d=4";
         String[] queryStringBits = queryString.split("&");
         System.out.println(Arrays.toString(queryStringBits) );
-        int a = 0;
-        int b = 0;
-        int c = 0;
+
+        List<String> paramNames = new ArrayList<>();
+        List<Integer> paramValues = new ArrayList<>();
 
         for(String bit : queryStringBits) {
             String[] bitBits = bit.split("=");
@@ -15,25 +17,22 @@ public class AppTest {
             String paramName = bitBits[0];
             String paramValue = bitBits[1];
 
-            if( paramName.equals("a") ) {
-                a = Integer.parseInt( paramValue );
-            }
-            else if( paramName.equals("b") ) {
-                b = Integer.parseInt( paramValue );
-            }
-            else if( paramName.equals("c") ) {
-                c = Integer.parseInt( paramValue );
-            }
+            paramNames.add(paramName);
+            paramValues.add(Integer.parseInt(paramValue));
 
             //System.out.printf("%s : %s\n", paramName, paramValue );
         }
-        System.out.printf("a : %d\n", a);
-        System.out.printf("b : %d\n", b);
-        System.out.printf("c : %d\n", c);
+
         /*
         Arrays.stream(queryStringBits)
                 .forEach(System.out::println);
          */
+
+        for(int i=0; i<paramNames.size(); i++) {
+            String paramName = paramNames.get(i);
+            int paramValue = paramValues.get(i);
+            System.out.printf("%s : %d\n", paramName, paramValue);
+        }
     }
 
 }
